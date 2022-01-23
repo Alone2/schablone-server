812c0ed7bd6c219afe84a22eb2468b6749a1ab62
Alone2 <alain.sinzig@gmail.com>
2022-01-23 17:53:45 +0100

    update project

--- scripts/schablone-server-client/import_templates.py
+++ scripts/schablone-server-client/import_templates.py
@@ -3,29 +3,26 @@
 import sys
 import xml.etree.ElementTree as ET
 
-import schablone_server_client.api.default as schablone
+from schablone_server_client.api.default import post_group_create, post_template_create
 from schablone_server_client import AuthenticatedClient
 
 client = None
 
 # Import XML
-async def importXML(xml_file_name: str):
+def importXML(xml_file_name: str):
     tree = ET.parse(xml_file_name)
     root = tree.getroot()
     # Groups
     for menu in root.iter("menu"):
         group_title = menu.find("title").text
 
-        response: int = await schablone.post_group_create.asyncio(client=client, name=group_title)
-        await schablone.post_group_add_parent_group.asyncio_detailed(
-            client=client, parent_group_id=1, group_id=response
-        )
-
+        response: int = post_group_create.sync(client=client, name=group_title, parent_group_id=1)
+        print("Added Group", group_title, "with ID", response)
         # Templates in these groups
         for texts in menu.iter("text"):
             template_content = ""
             template_name = ""
-            template_name = ""
+            template_subject = ""
             try:
                 template_content = texts.find("body").text
                 template_name = texts.find("name").text
@@ -33,22 +30,23 @@ async def importXML(xml_file_name: str):
             except:
                 pass
             # send to server
-            schablone.post_template_create.asyncio_detailed(
+            template_id : int = post_template_create.sync(
                 client=client,
                 name=template_name,
                 subject=template_subject,
                 content=template_content,
                 initial_group=response,
             )
-            # TODO: Send to correct group
-
+            print("Added Template", template_name, "with ID", template_id)
 
 # Start of script
 # Syntax ./import_templates.py ${BASE_URL_OF_SERVER} ${TOKEN} ${XML_FILES}
 if __name__ == "__main__":
     base_url = sys.argv[1]
     token = sys.argv[2]
-    client = AuthenticatedClient(base_url, token)
+    client = AuthenticatedClient(base_url, token=token)
+    client.headers = { "X-API-Key" : token}
+    print(client.get_headers())
     for i in range(3, len(sys.argv)):
         xml_path = sys.argv[i]
         importXML(xml_path)
